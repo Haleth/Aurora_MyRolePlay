a3002a0f86e7d2f5db34e693540b1fbfe6119008
Haleth <haleth@live.com>
2012-09-07 22:17:19 +0200

    Use our own class colours.

--- core.lua
+++ core.lua
@@ -1,6 +1,6 @@
 if not IsAddOnLoaded("FreeUI") and not IsAddOnLoaded("Aurora") then return end
 
-local F = unpack(Aurora or FreeUI)
+local F, C = unpack(Aurora or FreeUI)
 
 hooksecurefunc(mrp, "CreateBrowseFrame", function()
 	local bg = CreateFrame("Frame", nil, MyRolePlayBrowseFrame)
@@ -132,3 +132,38 @@ end
 
 hooksecurefunc(mrp, "CreateCFpfield", reskinHeader)
 hooksecurefunc(mrp, "CreateBFpfield", reskinHeader)
+
+local strsub, format, select = strsub, format, select
+
+local RAID_CLASS_COLORS_CODE = setmetatable( {}, { __index = function( table, key )
+	table[ key ] = C.classcolours[ key ] and format( "|cff%02x%02x%02x", C.classcolours[ key ].r * 255, C.classcolours[ key ].g * 255, C.classcolours[ key ].b * 255 ) or ""
+	return table[ key ]
+end } )
+
+local RPEVENTS = {
+	["CHAT_MSG_SAY"] = true,
+	["CHAT_MSG_EMOTE"] = true,
+	["CHAT_MSG_TEXT_EMOTE"] = true,
+	["CHAT_MSG_YELL"] = true,
+}
+
+local function mrp_GetColoredName( event, message, sender, language, arg4, arg5, arg6, arg7, arg8, arg9, arg10, lineid, guid )
+	if mrpSaved.Options.ShowRPNamesInChat and RPEVENTS[ event ] and sender and sender ~= UNKNOWN and msp.char[ sender ].supported and msp.char[ sender ].time.NA and mrp.DisplayChat.NA( msp.char[ sender ].field.NA ) ~= "" and mrp.DisplayChat.NA( msp.char[ sender ].field.NA ) ~= sender then
+		if ChatTypeInfo[ strsub( event, 10 ) or "" ] and ChatTypeInfo[ strsub( event, 10 ) or "" ].colorNameByClass and guid ~= "" then
+			return format( "%s%s|r", RAID_CLASS_COLORS_CODE[ ( select( 2, GetPlayerInfoByGUID( guid ) ) ) ], mrp.DisplayChat.NA( msp.char[ sender ].field.NA ) )
+		else
+			return mrp.DisplayChat.NA( msp.char[ sender ].field.NA )
+		end
+	else
+		return mrp_Prehook_GetColoredName( event, message, sender, language, arg4, arg5, arg6, arg7, arg8, arg9, arg10, lineid, guid )
+	end
+end
+
+function mrp:HookChatName()
+	mrp_Prehook_GetColoredName = GetColoredName
+	GetColoredName = mrp_GetColoredName
+end
+
+function mrp:UnhookChatName()
+	GetColoredName = mrp_Prehook_GetColoredName
+end
